e220f6d300d5c275daa409f3975900889195fb0f
Stepan Shtennikov <shtennikov.s@gmail.com>
2023-09-10 17:04:46 +0500

    refactor: format all files with new prettier config

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,11 @@
-<script setup lang="ts">
-</script>
-
 <template>
-  <div>
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus numquam pariatur fuga adipisci quasi autem
-    architecto nemo enim accusantium, itaque cumque nobis amet laudantium ratione provident repudiandae illum est
-    molestias.
-  </div>
+    <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus numquam pariatur fuga adipisci quasi autem
+        architecto nemo enim accusantium, itaque cumque nobis amet laudantium ratione provident repudiandae illum est
+        molestias.
+    </div>
 </template>
+
+<script setup lang="ts"></script>
 
 <style scoped></style>
