2dbe8c20461dc3c4344a9e51ffb48eb383ad40a7
Stepan Shtennikov <shtennikov.s@gmail.com>
2023-09-10 16:50:58 +0500

    chore: set up linting
    install eslint configs
    add eslint rules
    add .editorconfig

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,15 +1,53 @@
-/* eslint-env node */
 require('@rushstack/eslint-patch/modern-module-resolution')
 
 module.exports = {
   root: true,
-  'extends': [
+  extends: [
+    'airbnb-base',
     'plugin:vue/vue3-essential',
-    'eslint:recommended',
     '@vue/eslint-config-typescript',
-    '@vue/eslint-config-prettier/skip-formatting'
+    '@vue/eslint-config-prettier/skip-formatting',
+    'prettier'
   ],
+  parser: '@typescript-eslint/parser',
   parserOptions: {
-    ecmaVersion: 'latest'
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  plugins: ['@typescript-eslint'],
+  rules: {
+    'no-console': ['warn', { allow: ['warn', 'error', 'info'] }],
+
+    'import/prefer-default-export': 'off',
+    'import/no-extraneous-dependencies': 'off',
+
+    'max-lines-per-function': ['error', 40],
+    'class-methods-use-this': 'off',
+
+    // TypeScript
+    'lines-between-class-members': 'off',
+    '@typescript-eslint/no-inferrable-types': 'warn',
+    '@typescript-eslint/no-explicit-any': 'error',
+    '@typescript-eslint/explicit-function-return-type': 'error',
+    '@typescript-eslint/no-non-null-assertion': 'error',
+    '@typescript-eslint/array-type': [
+      'error',
+      {
+        default: 'array'
+      }
+    ],
+    '@typescript-eslint/explicit-member-accessibility': [
+      'error',
+      {
+        accessibility: 'explicit',
+        overrides: {
+          accessors: 'explicit',
+          constructors: 'off',
+          methods: 'explicit',
+          properties: 'explicit',
+          parameterProperties: 'explicit'
+        }
+      }
+    ]
   }
 }
