71e502cc4f68cfa73cdfbec7297b90ed092b1c05
Stepan Shtennikov <shtennikov.s@gmail.com>
2023-09-10 16:07:49 +0500

    chore: init vue project

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus numquam pariatur fuga adipisci quasi autem
+    architecto nemo enim accusantium, itaque cumque nobis amet laudantium ratione provident repudiandae illum est
+    molestias.
+  </div>
+</template>
+
+<style scoped></style>
