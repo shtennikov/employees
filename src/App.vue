7880950be69a18cc26b85005cbd6247dc2659f22
Stepan Shtennikov <shtennikov.s@gmail.com>
2023-09-16 16:46:47 +0500

    feat: add search bar component

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,10 @@
 <template>
-    <div>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus numquam pariatur fuga adipisci quasi autem
-        architecto nemo enim accusantium, itaque cumque nobis amet laudantium ratione provident repudiandae illum est
-        molestias.
-    </div>
+    <h2 class="search__title">Поиск</h2>
+    <UiSearchBar />
 </template>
 
 <script setup lang="ts">
-console.log(import.meta.env.VITE_SOME_KEY); // 123
-console.log(import.meta.env.DB_PASSWORD); // undefined
+import UiSearchBar from './components/UiSearchBar.vue';
 </script>
 
 <style scoped></style>
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import './assets/style/index.css';
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
 
