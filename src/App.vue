93437d7f6b3574b908e0fb6076b76330a795e6d7
Stepan Shtennikov <shtennikov.s@gmail.com>
2023-09-22 10:14:10 +0500

    feat: add BaseLayout component

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,37 @@
 <template>
-    <h2 class="search__title">Поиск</h2>
-    <UiSearchBar />
+    <BaseLayout>
+        <template #header>
+            <h2 class="search__title">Поиск</h2>
+            <UiSearchBar />
+        </template>
+
+        <template v-if="employees">
+            <UiPersonCard v-for="employee in employees" :key="employee.id" :person-data="employee" />
+        </template>
+
+        <template #footer>
+            <!-- footer -->
+        </template>
+    </BaseLayout>
 </template>
 
 <script setup lang="ts">
+import { ref, onMounted, type Ref } from 'vue';
+import type { PersonData } from './types/interfaces';
+import BaseLayout from './components/BaseLayout.vue';
 import UiSearchBar from './components/UiSearchBar.vue';
+import UiPersonCard from './components/UiPersonCard.vue';
+
+const employees: Ref<PersonData[] | null> = ref(null);
+
+onMounted(() => {
+    fetch('https://6505af3bef808d3c66f05196.mockapi.io/employees/')
+        .then((res) => res.json())
+        .then((resData) => {
+            employees.value = resData;
+        })
+        .catch((err) => console.error(err));
+});
 </script>
 
 <style scoped></style>
---
+++ src/components/BaseLayout.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="wrapper">
+        <header>
+            <slot name="header"></slot>
+        </header>
+
+        <main>
+            <slot name="default"></slot>
+        </main>
+
+        <footer>
+            <slot name="footer"></slot>
+        </footer>
+    </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped>
+.wrapper {
+    max-width: 1180px;
+    margin: auto;
+}
+</style>
